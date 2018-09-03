1cbda2433a08de9db5be3be28dbf5c22327b736c
sunzixiang <haichengsun123@163.com>
2018-09-03 18:24:50 +0800

    univalue test

--- test/no_nul.cpp
+++ test/no_nul.cpp
@@ -1,8 +1,14 @@
 #include "univalue.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main (int argc, char *argv[])
 {
     char buf[] = "___[1,2,3]___";
     UniValue val;
-    return val.read(buf + 3, 7) ? 0 : 1;
+    //return val.read(buf + 3, 7) ? 0 : 1;
+    cout << val.read(buf + 3, 7) << endl;
+    cout << val.write() << endl;
 }
--- test/test_json.cpp
+++ test/test_json.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-int main (int argc, char *argv[])
+int test_from_cin ()
 {
     UniValue val;
     if (val.read(string(istreambuf_iterator<char>(cin),
@@ -21,4 +21,109 @@ int main (int argc, char *argv[])
         cerr << "JSON Parse Error." << endl;
         return 1;
     }
+}
+
+
+int test_from_text ()
+{
+    UniValue val;
+    if (val.read("{\"a\":\"b\",\"hello\":[1,2,3,4,5]}")) 
+	{
+        cout << val.write(1 /* prettyIndent */, 4 /* indentLevel */) << endl;
+        return 0;
+    } else {
+        cerr << "JSON Parse Error." << endl;
+        return 1;
+    }
+}
+
+int test_from_text1 ()
+{
+    UniValue val;
+    if (val.read("{\"a\":\"b\",\"hello\":[1,2,3,4,5]}")) 
+	{
+        //cout << val.write(1 /* prettyIndent */, 4 /* indentLevel */) << endl;
+        cout << val.write(0,0) << endl;
+        return 0;
+    } else {
+        cerr << "JSON Parse Error." << endl;
+        return 1;
+    }
+}
+
+int test_push_back()
+{
+	UniValue blockHashes(UniValue::VARR);
+	blockHashes.push_back("abcdef");
+	blockHashes.push_back("12345");
+	blockHashes.push_back("789");
+	blockHashes.push_back(12345);
+
+	UniValue uniStr(UniValue::VSTR, "abcdef");
+	//UniValue uniStr(UniValue::VARR);
+	//uniStr.push_back("xyz");
+	//uniStr.push_back(123456);
+	cout << uniStr.write() << endl;
+
+	blockHashes.push_back(uniStr);
+
+	cout << uniStr.write() << endl;
+	cout << blockHashes.write() << endl;
+	cout << uniStr.isNum() << endl;
+	cout << uniStr.isStr() << endl;
+}
+
+int test_pushKV()
+{
+	UniValue blockHashes(UniValue::VARR);
+	blockHashes.push_back("abcdef");
+	blockHashes.push_back("12345");
+	blockHashes.push_back("789");
+	blockHashes.push_back(12345);
+
+	UniValue uniStr(UniValue::VOBJ);
+	uniStr.pushKV("hello", "world");
+	cout << uniStr.write() << endl;
+
+	blockHashes.push_back(uniStr);
+
+	cout << uniStr.write() << endl;
+	cout << blockHashes.write() << endl;
+}
+
+int test_pushKV1()
+{
+	//UniValue blockHashes(UniValue::VARR);
+	UniValue blockHashes1(UniValue::VOBJ);
+	UniValue blockHashes(UniValue::VARR);
+	blockHashes.push_back("abcdef");
+	blockHashes.push_back("12345");
+	blockHashes.push_back("789");
+	blockHashes.push_back(12345);
+
+	UniValue uniStr(UniValue::VOBJ);
+	uniStr.pushKV("hello", "world");
+	cout << uniStr.write() << endl;
+
+	//blockHashes.push_back(uniStr);
+	blockHashes1.pushKV("object", uniStr);
+	blockHashes1.pushKV("object1", blockHashes);
+
+	// 不生效
+	//blockHashes1.push_back("789");
+
+	cout << uniStr.write() << endl;
+	cout << blockHashes1.write() << endl;
+}
+
+
+
+int main (int argc, char *argv[])
+{
+	//test_from_cin();
+	//test_from_text();
+	//test_from_text1();
+	//test_push_back();
+	test_pushKV1();
+	return 0;
 }
